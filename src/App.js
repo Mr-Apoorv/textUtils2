7a71e5c7055b04f0218a9a11fc59334a9fe2d30e
Apoorv <dixit.a.apoorv@gmail.com>
2022-08-20 17:58:00 +0530

    L-10 Creating enable dark mode button using useState Hook

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
+import About from "./components/About";
 import NavBar from "./components/NavBar";
-import TextForm from "./components/TextForm";
+// import TextForm from "./components/TextForm";
 
 function App() {
   return (
@@ -8,8 +9,9 @@ function App() {
       {/* <NavBar title="Text-Utils" aboutText="About-textUtils"/> */}
       {/* <NavBar/> */}
       <NavBar title="Text-Utils" />
-      <div className="container">  
-      <TextForm heading="Enter your text below to analyze"/>
+      <div className="container">
+        {/* <TextForm heading="Enter your text below to analyze" /> */}
+        <About />
       </div>
     </>
   );
